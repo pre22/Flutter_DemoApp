5a6cbf33dae89bd1e432bed7b540a87c600d0161
Pre22 <pre.chika.22@gmail.com>
2020-09-05 14:08:48 +0100

    A screen with a text at the middle

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import './screens/home.dart';
 
+void main() => runApp(new HelloFlutterApp());
+
+class HelloFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // This removes the debug banner
+      debugShowCheckedModeBanner: false,
+      // This widget title adds a title to our appbar
+      title: "Hello Flutter App",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Title in App Bar"),
+          ),
+          // This houses the body container
+          body: Home()),
+    );
+  }
+}
